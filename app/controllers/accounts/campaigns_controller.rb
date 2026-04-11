1b0e8ebe7d9b08b231d1a6750ec5ad976b7e388e
achadinhospromosdagleici-dev <achadinhospromosdagleici@gmail.com>
2026-04-10 23:47:53 -0300

    fix: 500 error in campaign automation screen and permit parameters

--- app/controllers/accounts/campaigns_controller.rb
+++ app/controllers/accounts/campaigns_controller.rb
@@ -271,6 +271,7 @@ class Accounts::CampaignsController < InternalController
       :name, :campaign_category_id, :pipeline_id, :stage_id, 
       :insert_ddi, :ai_randomization, :current_step, 
       :batch_delay, :inbox_rotation_rule, :max_delay,
+      :human_intervention_lock, :stop_words, :failover_inbox_id, :roi_conversion_value,
       { chatwoot_inbox_ids: [] }
     ]
 
