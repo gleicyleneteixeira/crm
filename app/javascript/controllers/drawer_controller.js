566fed74454bab75f7433dbe80329d7e60582f1d
Yukio <45243355+YukioArie@users.noreply.github.com>
2026-04-21 18:16:16 -0300

    Fix drawer backdrop bugs (#625)
    * Fix prevent drawer backdrop when navigate between pages
    
    * Fix click on drawer backdrop behaviour
    
    * Refactor

--- app/javascript/controllers/drawer_controller.js
+++ app/javascript/controllers/drawer_controller.js
@@ -3,12 +3,13 @@ import { Drawer } from "flowbite";
 
 export default class extends Controller {
   connect() {
+    this.removeStaleBackdrops();
     this.drawer = new Drawer(this.element, {
       placement: "right",
       backdrop: true,
       bodyScrolling: false,
       backdropClasses:
-        "bg-gray-900/50 dark:bg-gray-900/80 fixed inset-0 z-50 pointer-events-none drawer-backdrop",
+        "bg-gray-900/50 dark:bg-gray-900/80 fixed inset-0 z-50 drawer-backdrop",
       onHide: () => {
         setTimeout(() => {
           this.element.remove();
@@ -17,11 +18,16 @@ export default class extends Controller {
     });
     setTimeout(() => {
       this.drawer.show();
-      this.preventBackdropAfterMorphRefresh()
+      this.preventBackdropAfterMorphRefresh();
+      this.preventBackdropClickFromClosingDrawer();
     }, 100);
   }
   disconnect() {
     this.drawer.hide();
+    this.removeStaleBackdrops();
+  }
+  removeStaleBackdrops() {
+    document.querySelectorAll(".drawer-backdrop").forEach((el) => el.remove());
   }
   drawerHide(event) {
     event.preventDefault();
@@ -31,9 +37,18 @@ export default class extends Controller {
     event.preventDefault();
   }
   preventBackdropAfterMorphRefresh() {
-    const backdrop = document.getElementsByClassName("drawer-backdrop")[0];
-    if (backdrop) {
-      backdrop.dataset.turboPermanent = true;
+    if (this.backdrop) {
+      this.backdrop.dataset.turboPermanent = true;
     }
+  }
+  preventBackdropClickFromClosingDrawer() {
+    this.backdrop.addEventListener(
+      "click",
+      (event) => event.stopImmediatePropagation(),
+      true,
+    );
+  }
+  get backdrop() {
+    return document.getElementsByClassName("drawer-backdrop")[0];
   }
 }
